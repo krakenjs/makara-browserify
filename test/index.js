baa0ae10ad0cdccf9306c202dd120b5f93d8e0e9
Aria Stewart <aredridel@dinhe.net>
2014-11-19 16:49:30 -0500

    Add tests for build process

---
+++ test/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var test = require('tape');
+var path = require('path');
+var fs = require('fs');
+
+var bksb = require('../');
+
+test('build', function (t) {
+    var root = path.resolve(__dirname, 'fixture');
+    bksb.build(root, function (err) {
+        t.error(err, 'no error');
+
+        fs.readFile(path.resolve(root, '.build/en-XC/_languagepack.js'), function (err, data) {
+            t.error(err, 'no error loading result');
+
+            var result;
+            eval(data + '; result = require("_languagepack")');
+
+            t.ok(result['en-XC'], "found our language in the output");
+            t.equal(result['en-XC']['index.properties'].hello, 'World', "found translation");
+        });
+
+    });
+    t.end();
+});
