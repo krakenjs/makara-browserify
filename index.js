d7d758b4d648c309d2cff13839d2915a3d104348
Aria Stewart <aredridel@dinhe.net>
2015-04-03 16:38:23 -0400

    Move languagepackpath stuff out to a module

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 "use strict";
 
 var bcp47 = require('bcp47');
+var mlpp = require('makara-languagepackpath');
 
 module.exports = {
     build: function (root, cb) {
         require('./build')(root, cb);
     },
-    languagePackPath: languagePackPath,
-    middleware: function() {
-        return function (req, res, next) {
-            if (!res.locals.makara) res.locals.makara = {};
-            res.locals.makara.languagePackPath = languagePackPath(res.locals.locale);
-            next();
-        };
-    }
-
+    languagePackPath: mlpp.languagePackPath,
+    middleware: mlpp.middleware
 };
-
-function languagePackPath(locale) {
-    if (!locale) throw new Error("Must specify a locale");
-    // Handle PayPal-style input
-    if (locale.language && locale.country) locale = locale.language + '-' + locale.country;
-
-    // Handle strings
-    if (typeof locale == 'string') locale = bcp47.parse(locale);
-
-    if (!locale || !locale.langtag || !locale.langtag.language || !locale.langtag.language.language || !locale.langtag.region) throw new Error('Invalid locale');
-
-    return locale.langtag.language.language + '-' + locale.langtag.region + '/_languagepack.js';
-}
